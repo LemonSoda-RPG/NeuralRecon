c3885f376de5f34ad89539b0a1415238f3e3f72e
jiacheng <1178010161@qq.com>
2023-11-07 11:32:59 +0800

    diyici

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,8 +4,8 @@ import importlib
 # find the dataset definition by name, for example ScanNetDataset (scannet.py)
 def find_dataset_def(dataset_name):
     module_name = 'datasets.{}'.format(dataset_name)
-    module = importlib.import_module(module_name)
+    module = importlib.import_module(module_name)  #先获取文件  之后再返回文件当中的类
     if dataset_name == 'scannet':
-        return getattr(module, "ScanNetDataset")
+        return getattr(module, "ScanNetDataset")   #返回ScanNetDataset类
     elif dataset_name == 'demo':
-        return getattr(module, "DemoDataset")
+        return getattr(module, "DemoDataset")  #返回DemoDataset类
